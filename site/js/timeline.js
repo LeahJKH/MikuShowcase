cbeb674148e3db7becda84e889a715b91b585fab
LeahJKH <Leahb06.kodehode@gmail.com>
2024-08-28 14:19:02 +0200

    did all the logic

---
+++ site/js/timeline.js
@@ -0,0 +1,28 @@
+const url = "https://raw.githubusercontent.com/LeahJKH/MikuApiGithub/main/json/mikuTimeLine.json"
+const cont = document.querySelector("#slider")
+
+fetch(url)
+    .then(response => response.json())
+    .then(timeline => {
+        createTime(timeline)
+    })
+    .catch(error => {
+        console.error('Error fetching the JSON data:', error);
+    });
+
+function createTime(t) {
+    for(let i = 0; i <= t.length; i++){
+        const div = document.createElement("div")
+        div.classList = "card"
+        const h2 = document.createElement("h2")
+        const htxt = document.createTextNode(t[i].time)
+        h2.append(htxt)
+        div.appendChild(h2)
+
+        const p = document.createElement("p")
+        const ptxt = document.createTextNode(t[i].event)
+        p.append(ptxt)
+        div.appendChild(p)
+        cont.appendChild(div)
+    } 
+}
