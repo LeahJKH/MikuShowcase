f0eeccaf3ead36e409d8407ad1e2468e44554a1b
LeahJKH <Leahb06.kodehode@gmail.com>
2024-08-28 11:07:57 +0200

    queality of life update

--- site/js/app.js
+++ site/js/app.js
@@ -1,12 +1,44 @@
-document.addEventListener("DOMContentLoaded", (e) => {
+const Sites = {
+    images: { 
+        Text: "Images", 
+        id: "imgBtn",
+        site: "images",
+    },
+    vocaloids: {
+        Text: "Vocaloids", 
+        id: "vocBtn",
+        site: "vocaloids",
+    }
+}
+const btnCont = document.querySelector("#btn-cont");
 
-    const imgBtn = document.querySelector("#imgIdentifier")
-    const jsonBtn = document.querySelector("#jsonIdentifier")
+function createBtn(Txt, btnID, site) {
+   
+    const sect = document.createElement("section");
+    sect.classList.add("main-sect-cont");  
     
-    imgBtn.addEventListener("click", (e) => {
-        location.href = "./site/pages/images.html"
-    })
-    jsonBtn.addEventListener("click", (e) => {
-        location.href = "./site/pages/json.html"
-    })
-})
+  
+    const h3 = document.createElement("h3");
+    const title = document.createTextNode(`${Txt}`);
+    h3.append(title);
+    
+    
+    const btn = document.createElement("button");
+    btn.classList.add("main-btn-PS");  
+    btn.id = `${btnID}`;
+    btn.textContent = `Go to ${Txt}`; 
+
+
+    btn.addEventListener("click", (e) => {
+            location.href = `./site/pages/${site}.html`;
+    });
+
+
+    sect.appendChild(h3);
+    sect.appendChild(btn);
+    btnCont.appendChild(sect);
+}
+
+createBtn(Sites.images.Text, Sites.images.id, Sites.images.site);
+createBtn(Sites.vocaloids.Text, Sites.vocaloids.id, Sites.vocaloids.site);
+
